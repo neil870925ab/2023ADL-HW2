94b8bb0dbdc12d389327bd5c6829ab3de81d4610
neil870925ab <52836478+neil870925ab@users.noreply.github.com>
2023-11-08 15:29:22 +0800

    Add files via upload

--- download.sh
+++ download.sh
@@ -1,4 +1,4 @@
-wget "https://www.dropbox.com/scl/fo/rnkuazmho80gbxsvb62ot/h?rlkey=5x9cqnuxqivatd0ty2ecbmb0n&dl=1" -O download.zip
+wget "https://www.dropbox.com/scl/fo/bl46s5mdmzoeo6kmtx1ov/h?rlkey=cyqi7uvtjx59g8l7mz0670wic&dl=1" -O download.zip
   
 unzip download.zip
 
